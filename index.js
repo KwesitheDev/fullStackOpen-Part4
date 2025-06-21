5d4380aa433afde8019b86c0f70e55f25c87e665
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-06-21 03:47:11 +0100

    refactored app

--- index.js
+++ index.js
@@ -1,34 +1,6 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-const Blog = require('./models/blogSchema')
-
-dotenv.config()
-
-const app = express()
-
-
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
-
-app.use(express.json())
-
-app.get('/api/blogs', (req, res) => {
-    Blog.find({}).then(blogs => {
-        res.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (req, res) => {
-    const blog = new Blog(req.body)
-
-    blog.save().then(result => {
-        res.status(201).json(result)
-    })
-})
-
-const PORT = process.env.PORT
+const app = require('./app')
+const config = require('./utils/config')
+const PORT = config.PORT
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
