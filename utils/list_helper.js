960401147bc95c185c27584a9af6450a3eff29be
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-06-21 17:41:41 +0100

    total likes added

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -4,4 +4,19 @@ const dummy = (blogs) => {
     return 1;
 }
 
-export default dummy
+const totalLikes = (blogs) => {
+    let total = 0
+
+    //blogs.reduce((sum,blog)=> sum+blog.likes,0)
+    /*blogs.forEach(blog => {
+        total +=blog.likes
+    });*/
+    
+    for (const blog of blogs) {
+        total +=blog.likes
+    }
+
+    return total
+}
+
+export default {dummy, totalLikes}
