34ad0c3e977e1336b1c5036dec7f44044b4227af
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-06-21 03:13:46 +0100

    Schema refactored and imported

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const Blog = require('./models/blogSchema')
 
 dotenv.config()
 
 const app = express()
 
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes:Number,
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
