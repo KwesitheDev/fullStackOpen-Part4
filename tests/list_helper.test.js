88f03bc6f8737f0fb7cfbaace21998a728edd433
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-06-28 02:31:11 +0100

    removed unknown import (constants)

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,7 +1,6 @@
 const { test, describe } = require('node:test');
 const assert = require ('node:assert')
 const listHelper = require ('../utils/list_helper.js');
-const { constants } = require('node:fs/promises');
 
 
 test('dummy returns one', () => {
