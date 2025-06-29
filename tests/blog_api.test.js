d7f36df1957cdf92bd7b655790f61a7c6e3b0800
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-06-29 01:56:34 +0100

    trimed logic and added describe

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
+const helper = require('../utils/blog_api_helper')
 const Blog = require('../models/blogSchema')
 const config = require('../utils/config')
 
@@ -11,59 +12,49 @@ before(async () => {
 })
 
 beforeEach(async () => {
-    await Blog.deleteMany({})
+    await helper.resetDbWithInitialBlog()
+})
 
-    const blog = new Blog({
-        title: 'test',
-        author: 'KwesitheDev',
-        url: 'kwesithedev.me',
-        likes: 50
+describe('when there is initially one blog in the db', () => {
+    test('blogs are returned as JSON and contain id property', async () => {
+        const response = await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        if (!response.body[0].id) {
+            throw new Error('Blog does not have id property')
+        }
     })
 
-    await blog.save()
+    test('a new blog can be added with POST', async () => {
+        const initialBlogs = await helper.blogInDb()
+
+        const newBlog = {
+            title: 'another test',
+            author: 'KwesitheDev',
+            url: 'kwesithedev.me',
+            likes: 15
+        }
+
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const allBlogs = await helper.blogInDb()
+
+        if (allBlogs.length !== initialBlogs.length + 1) {
+            throw new Error('Blog count did not increase')
+        }
+
+        const titles = allBlogs.map(b => b.title)
+        if (!titles.includes(newBlog.title)) {
+            throw new Error('New blog title not found in database')
+        }
+    })
 })
-
-test('blog posts have property id', async () => {
-    const response = await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-
-})
-
-test('blog can be added via post', async () => {
-    const initialBlogs = await Blog.find({})
-
-    const newBlog = {
-        title: 'another test',
-        author: 'KwesitheDev',
-        url: 'kwesithedev.me',
-        likes: 15
-    }
-
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-    
-    
-    const allBlogs = await Blog.find({})
-
-    console.log(`Current length: ${allBlogs.length} ---- Previous length: ${initialBlogs.length}`)
-    if (allBlogs.length !== initialBlogs.length + 1) {
-        throw new Error('Blog count did not increase')
-    }
-
-    
-    const titles = allBlogs.map(b => b.title)
-    if (!titles.includes(newBlog.title)) {
-        throw new Error('New blog title not found in database')
-    }
-})
-
-
-
 
 after(async () => {
     await mongoose.connection.close()
