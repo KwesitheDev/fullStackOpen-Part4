f88e27774224105eeefdbd59ca7a82b4790bba72
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-06-30 04:00:20 +0100

    Added put functionality to update blog

--- controllers/blog.js
+++ controllers/blog.js
@@ -18,6 +18,22 @@ blogsRouter.delete('/:id', async (req, res) => {
     const id = req.params.id
     await Blog.findByIdAndDelete(id)
     res.status(204).end()
+
+})
+
+blogsRouter.put('/:id', async (req, res) => {
+    
+    const body = req.body
+    const id = req.params.id
+
+    const blog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+    }
+    const updatedBlog = await Blog.findByIdAndUpdate(id,blog,{new:true})
+    res.json(updatedBlog)
 })
 
 
