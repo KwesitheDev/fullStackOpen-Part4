d357cde9895b3f2aff7a2ceea32830bd36200ae2
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-07-03 02:40:46 +0100

    Users.js

---
+++ models/Users.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        required: true,
+        type: String, 
+    },
+    username: {
+        required: true, 
+        unique: true,
+        type: String
+    },
+    passwordHash
+})
+
+userSchema.set('toJson', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id
+        delete returnedObject._id
+        delete returnedObject.__v
+        delete returnedObject.passwordHash
+    }
+})
+
+const User = mongoose.model('user', userSchema)
+
+module.exports = {
+    User
+}
