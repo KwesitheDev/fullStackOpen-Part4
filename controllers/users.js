b0f7a2da2d11accd112b167de6aeb2320db54f35
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-07-04 01:20:35 +0100

    checks for username and password validation

--- controllers/users.js
+++ controllers/users.js
@@ -10,6 +10,25 @@ usersRouter.get('/', async (req, res) => {
 usersRouter.post('/', async (req, res) => {
     const { name, username, password } = req.body
 
+    //check for username and password not missing
+    if (!username || !password) {
+        return res
+                .status(400)
+                .json({ error: 'username and password are required' })
+
+    }
+
+    //Check for length of username & password
+    if (username.length < 3 || password.length < 3) {
+        return res.status(400).json({ error: 'username and password must be at least 3 characters long' })
+    }
+
+    //Check for unique username
+    const existingUser = await User.findOne({ username })
+    if (existingUser) {
+        return res.status(400).json({ error: 'username must be unique' })
+    }
+
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(password, saltRounds)
 
