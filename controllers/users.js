d590783b6902504e735747291903bda560b868bc
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-07-04 01:41:58 +0100

    added populate to get route

--- controllers/users.js
+++ controllers/users.js
@@ -3,9 +3,16 @@ const  User  = require('../models/Users')
 const bcrypt = require('bcrypt')
 
 usersRouter.get('/', async (req, res) => {
-    const users = await User.find({})
+    const users = await User.find({}).populate('blogs', {
+        title: 1,
+        url: 1,
+        author: 1
+    })
+
     res.json(users)
 })
+
+
 
 usersRouter.post('/', async (req, res) => {
     const { name, username, password } = req.body
